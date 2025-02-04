000f40c7fabe407178e24143f2d2a185ff386476
lallen21 <156361246+lallen21@users.noreply.github.com>
2025-02-04 19:59:41 +0000

    Added *New fav icn *added color kit *sparkle

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Okay👍');
+
+
+const body = document.body;
+console.log(body);
+
+// Set the background color of the body using colorkit
+body.style.backgroundColor = colorkit.random();
+// console.log(body.style.backgroundColor);
+
+// Apply colorkit sparkle effect to the body
+colorkit.sparkle(body);
